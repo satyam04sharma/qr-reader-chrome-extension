fa5306b08fab234c46693f9e0f1a87cfe0fbd9f7
Satyam Sharma <satyam04sharma@gmail.com>
2018-09-01 01:46:29 +0530

    extention created

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $("#btn1").click(function() {
-        $.get("https://api.qrserver.com/v1/create-qr-code/?data=" + encodeURIComponent($("#text")), function(data, status) {
-            chrome.browserAction.onClicked.addListener(alert(echo(" " + data + " " + status)));
-        });
+        var res = "https://api.qrserver.com/v1/create-qr-code/?data=" + $("#text").val() + "&size=60x60";
+        $("#image").attr('src', res);
+        /*$.get("https://api.qrserver.com/v1/create-qr-code/?data=" + encodeURIComponent($("#text") + "&size=60x60"), function(data, status) {
+            $("#image").attr('src', 'data');
+        });*/
     });
 });
